a4c3cc98488195df476e31259a43526dc9935d06
HWN <heatherwensler@gmail.com>
2017-08-03 11:07:27 -0700

    function response exists

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const bodyParser = require('body-parser')
 const request = require('request');
 const app = express()
 
+//set ports - enviroment variable OR localhost 5000
 app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+//function has a request and a response
+app.get('/', function (req, res){
+  res.send('Hi!')
+})
