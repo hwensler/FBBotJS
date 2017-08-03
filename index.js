0f37d997f3e243a9de2ed9800ef9836df4c7222b
HWN <heatherwensler@gmail.com>
2017-08-03 10:30:27 -0700

    added app enviroment variables and port

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request');
 const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
