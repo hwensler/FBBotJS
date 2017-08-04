07fc6020d7d4b1c852ff994320b135060f3cfdbc
HWN <heatherwensler@gmail.com>
2017-08-04 11:24:02 -0700

    taking out the weird psid attempt which broke my site

--- index.js
+++ index.js
@@ -37,17 +37,17 @@ app.get('/webhook/', function(req, res){
      res.send('No entry')
 })
 
-//attempting to display PSID
-app.get('/PSID/', function(req, res){
-    MessengerExtensions.getUserID(function success(uids) {
-        //userID was obtained
-        var psid = uids.psid
-    }
-    function error(err, errorMessage){
-        //error handling code
-
-    }
-})
+// //attempting to display PSID
+// app.get('/PSID/', function(req, res){
+//     MessengerExtensions.getUserID(function success(uids) {
+//         //userID was obtained
+//         var psid = uids.psid
+//     }
+//     function error(err, errorMessage){
+//         //error handling code
+//
+//     }
+// })
 
 //add server
 app.listen(app.get('port'), function(){
