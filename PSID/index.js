60477a17b8c9828f215d43c987cc87ec168a2ba2
HWN <heatherwensler@gmail.com>
2017-08-04 11:43:49 -0700

    attempting to make psid page actually work

--- PSID/index.js
+++ PSID/index.js
@@ -1,3 +1,16 @@
+//npm packages
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request');
+const app = express()
+
+//set ports - environment variable OR localhost 5000
+app.set('port', (process.env.PORT || 5000))
+
+//use json parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 //generic display
 app.get('/', function (req, res){
     res.send('A PSID will display here someday.')
